1e99153aeb4f23fdee8c9c5a6acf18ede5c6936a
Sam Verschueren <sam.verschueren@gmail.com>
2015-06-25 15:40:30 +0200

    Using AuthToken now instead of credentials

--- run.sh
+++ run.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-set -x
-
 if [ -z "$WERCKER_NPM_PRIVATE_REGISTRY_HOST" ]; then
+	# Stop execution if the host is not provided
 	fail "Please provide a private registry host"
 fi
 
 if [[ $WERCKER_NPM_PRIVATE_REGISTRY_HOST == http* ]]; then
+	# If the host starts with http, use it as is
 	REGISTRY=$WERCKER_NPM_PRIVATE_REGISTRY_HOST
 	npm set registry $REGISTRY
 else
+	# Prepend the host with http:// if it was not set
 	REGISTRY=http://$WERCKER_NPM_PRIVATE_REGISTRY_HOST
 	npm set registry $REGISTRY
 fi
 
-if [ -n "$WERCKER_NPM_PRIVATE_REGISTRY_USERNAME" ] && [ -n "$WERCKER_NPM_PRIVATE_REGISTRY_PASSWORD" ] && [ -n "$WERCKER_NPM_PRIVATE_REGISTRY_EMAIL" ]; then
-npm adduser --registry $REGISTRY <<EOF
-$WERCKER_NPM_PRIVATE_REGISTRY_USERNAME
-$WERCKER_NPM_PRIVATE_REGISTRY_PASSWORD
-$WERCKER_NPM_PRIVATE_REGISTRY_EMAIL
-EOF
+if [ -n "$WERCKER_NPM_PRIVATE_REGISTRY_AUTH_TOKEN" ]; then
+	# Append the token to the ~/.npmrc file
+	echo $WERCKER_NPM_PRIVATE_REGISTRY_AUTH_TOKEN >> ~/.npmrc
 fi
-
-set +x
