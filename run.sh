fb4acb7fd9ba22923fa1fdcc0e4e4b64f82afae2
Sam Verschueren <sam.verschueren@gmail.com>
2015-06-25 13:55:26 +0200

    Fixed pattern matching

--- run.sh
+++ run.sh
@@ -4,11 +4,16 @@ if [ -z "$WERCKER_NPM_PRIVATE_REGISTRY_HOST" ]; then
 	fail "Please provide a private registry host"
 fi
 
-if [ $WERCKER_NPM_PRIVATE_REGISTRY_HOST == http* ]; then
+if [[ $WERCKER_NPM_PRIVATE_REGISTRY_HOST == http* ]]; then
+	echo "Registry starts with http"
+
 	REGISTRY=$WERCKER_NPM_PRIVATE_REGISTRY_HOST
 	npm set registry $REGISTRY
 else
 	REGISTRY=http://$WERCKER_NPM_PRIVATE_REGISTRY_HOST
+	
+	echo $REGISTRY
+	
 	npm set registry $REGISTRY
 fi
 
