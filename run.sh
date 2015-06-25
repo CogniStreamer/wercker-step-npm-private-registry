99de361ac85463e5f3bd0957f1a1c3cb93f336ad
Sam Verschueren <sam.verschueren@gmail.com>
2015-06-25 13:12:35 +0200

    Updated run script

--- run.sh
+++ run.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-if [ -z "$WERCKER_NPM_REGISTRY_HOST" ]; then
-	if [ $WERCKER_NPM_REGISTRY_HOST === http*]; then
-		REGISTRY=$WERCKER_NPM_REGISTRY_HOST
-		npm set registry $REGISTRY
-	else
-		REGISTRY=http://$WERCKER_NPM_REGISTRY_HOST
-		npm set registry $REGISTRY
-	fi
+if [ -z "$WERCKER_NPM_PRIVATE_REGISTRY_HOST" ]; then
+	fail "Please provide a registry host"
 fi
 
-if [ -z "$WERCKER_NPM_REGISTRY_USERNAME" ]; then
+if [ -z "$WERCKER_NPM_PRIVATE_REGISTRY_USERNAME" ]; then
 	fail "Please provide a username"
 fi
 
-if [ -z "$WERCKER_NPM_REGISTRY_PASSWORD" ]; then
+if [ -z "$WERCKER_NPM_PRIVATE_REGISTRY_PASSWORD" ]; then
 	fail "Please provide a password"
 fi
 
-if [ -z "$WERCKER_NPM_REGISTRY_EMAIL" ]; then
+if [ -z "$WERCKER_NPM_PRIVATE_REGISTRY_EMAIL" ]; then
 	fail "Please provide an email address"
 fi
 
-if [ -z "$REGISTRY" ]; then
-npm adduser <<EOF
-$WERCKER_NPM_REGISTRY_USERNAME
-$WERCKER_NPM_REGISTRY_PASSWORD
-$WERCKER_NPM_REGISTRY_EMAIL
-EOF
+if [ $WERCKER_NPM_REGISTRY_HOST === http*]; then
+	REGISTRY=$WERCKER_NPM_REGISTRY_HOST
+	npm set registry $REGISTRY
 else
+	REGISTRY=http://$WERCKER_NPM_REGISTRY_HOST
+	npm set registry $REGISTRY
+fi
+
 npm adduser --registry $REGISTRY <<EOF
 $WERCKER_NPM_REGISTRY_USERNAME
 $WERCKER_NPM_REGISTRY_PASSWORD
 $WERCKER_NPM_REGISTRY_EMAIL
 EOF
-fi
