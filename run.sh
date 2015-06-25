f44b24f6dac4c87ad4f2bd28c7b7d079977d805b
Sam Verschueren <sam.verschueren@gmail.com>
2015-06-25 16:17:13 +0200

    Fixed authToken appending to npmrc

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@ if [ -z "$WERCKER_NPM_PRIVATE_REGISTRY_HOST" ]; then
 	fail "Please provide a private registry host"
 fi
 
+if [ -e "~/.npmrc" ]; then
+	# Remove existing .npmrc file if it exists
+	rm ~/.npmrc
+fi
+
 if [[ $WERCKER_NPM_PRIVATE_REGISTRY_HOST == http* ]]; then
 	# If the host starts with http, use it as is
 	REGISTRY=$WERCKER_NPM_PRIVATE_REGISTRY_HOST
@@ -17,5 +22,13 @@ fi
 
 if [ -n "$WERCKER_NPM_PRIVATE_REGISTRY_AUTH_TOKEN" ]; then
 	# Append the token to the ~/.npmrc file
-	echo $WERCKER_NPM_PRIVATE_REGISTRY_AUTH_TOKEN >> ~/.npmrc
+	
+	# Remove http: or https:
+	TEMP = ${REGISTRY#*:}
+	
+	# Remove trailing slash
+	TEMP = ${TEMP%/}
+	
+	# Write the line to the ~/.npmrc file
+	echo "$TEMP/:_authToken=\"$WERCKER_NPM_PRIVATE_REGISTRY_AUTH_TOKEN\"" >> ~/.npmrc
 fi
